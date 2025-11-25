35125a0afcf7d53c705e2273e493339aa9a54cff
premgurjar7 <pgbhai259@gmail.com>
2025-11-25 17:31:45 +0530

    wertyui

--- server.js
+++ server.js
@@ -45,5 +45,5 @@ app.get("/", (req, res) => {
 });
 
 // start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
